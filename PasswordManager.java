2a4e4781ff7e359ebd3396efc8a1acb0a1e26b48
nurdanish22 <nurdanish22@gmail.com>
2023-12-12 18:07:52 +0800

    Updated some comments for the PasswordManager.java for clarification

--- PasswordManager.java
+++ PasswordManager.java
@@ -1,10 +1,10 @@
 package PasswordManager;
 
 import java.util.ArrayList;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.nio.charset.StandardCharsets;
-import java.math.BigInteger;
+import java.security.MessageDigest; //This is to hash the password
+import java.security.NoSuchAlgorithmException; //This is to check if the algorithm is valid
+import java.nio.charset.StandardCharsets; //This is to convert the password to bytes
+import java.math.BigInteger; //This is to convert the bytes to a BigInteger
 
 public class PasswordManager {
     private ArrayList<PasswordEntry> passwordEntries;
@@ -17,15 +17,15 @@ public class PasswordManager {
 
     private String hashPassword(String password) {
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            byte[] messageDigest = md.digest(password.getBytes(StandardCharsets.UTF_8));
+            MessageDigest md = MessageDigest.getInstance("SHA-512"); //This is the hashing algorithm
+            byte[] messageDigest = md.digest(password.getBytes(StandardCharsets.UTF_8)); //This converts the password to bytes and hashes it
             BigInteger no = new BigInteger(1, messageDigest);
-            String hashtext = no.toString(16);
-            while (hashtext.length() < 32) {
+            String hashtext = no.toString(16); //This converts the bytes to a BigInteger and then to a string
+            while (hashtext.length() < 32) { //This adds a 0 to the beginning of the string if it is too short
                 hashtext = "0" + hashtext;
             }
             return hashtext;
-        } catch (NoSuchAlgorithmException e) {
+        } catch (NoSuchAlgorithmException e) { //This is to catch if the algorithm is invalid
             throw new RuntimeException(e);
         }
     }
