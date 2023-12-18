d3a02157fecf447da9c4d542969609a52de6acab
IzzuddinNurasheed <cytusizz127@gmail.com>
2023-12-18 14:19:05 +0800

    testing.2

--- PasswordManager.java
+++ PasswordManager.java
@@ -47,3 +47,4 @@ public class PasswordManager { // Nurdanish (2224875)
         return new ArrayList<>(this.passwordEntries); // Return a copy of the passwordEntries ArrayList to ensure that the original ArrayList is not modified (encapsulation)
     }
 } //i like men
+//nicholas cage
